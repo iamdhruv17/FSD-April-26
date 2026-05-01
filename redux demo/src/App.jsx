66407004a2b0fa07aef8538bda87fcf4e7b602af
iamdhruv17 <dsinghal7834@gmail.com>
2026-05-01 15:53:15 +0530

    01-05-2026

---
+++ redux demo/src/App.jsx
@@ -0,0 +1,13 @@
+  import Todo from './components/todo.jsx'
+
+export default function App()
+{
+    return(
+        <div>
+            <h1>
+                TODO APP With Redux
+            </h1>
+            <Todo />
+        </div>
+    )
+}
